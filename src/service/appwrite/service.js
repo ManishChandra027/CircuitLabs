a10c6892e2bce1b825bdf71e5d345470c24ff8c9
ManishChandra027 <ManishChandra027>
2026-04-05 16:59:35 +0530

    complete service db

--- src/service/appwrite/service.js
+++ src/service/appwrite/service.js
@@ -13,29 +13,95 @@ class Service {
   }
 
   async createContact({ name, phone, email, address }) {
-    const result = await databases.createDocument({
-      databaseId: config.appwriteDatabaseId,
-      collectionId: config.appwriteCollectionId,
-      documentId: ID.unique(),
-      data: {
-        name,
-        email,
-        phone,
-        address,
-      },
-    });
+    try {
+      const result = await this.databases.createDocument({
+        databaseId: config.appwriteDatabaseId,
+        collectionId: config.appwriteCollectionId,
+        documentId: ID.unique(),
+        data: {
+          name,
+          email,
+          phone,
+          address,
+        },
+      });
+      if (result) {
+        console.log(result);
+        return result;
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
-  async createContact({ name, phone, email, address }) {
-    const result = await databases.createDocument({
-      databaseId: config.appwriteDatabaseId,
-      collectionId: config.appwriteCollectionId,
-      documentId: ID.unique(),
-      data: {
-        name,
-        email,
-        phone,
-        address,
-      },
-    });
+  async getContact(id) {
+    try {
+      const result = await this.databases.getDocument({
+        databaseId: config.appwriteDatabaseId,
+        collectionId: config.appwriteCollectionId,
+        documentId: id,
+        queries: [], // optional
+      });
+      if (result) {
+        console.log(result);
+        return result;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async getContacts(id, queries = []) {
+    try {
+      const result = await this.databases.listDocuments({
+        databaseId: config.appwriteDatabaseId,
+        collectionId: config.appwriteCollectionId,
+
+        queries, // optional
+      });
+      if (result) {
+        console.log(result);
+        return result;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async updateContact(id, { name, phone, email, address }) {
+    try {
+      const result = await this.databases.updateDocument({
+        databaseId: config.appwriteDatabaseId,
+        collectionId: config.appwriteCollectionId,
+        documentId: id,
+        data: {
+          name,
+          email,
+          phone,
+          address,
+        },
+      });
+      if (result) {
+        console.log(result);
+        return result;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async deleteContact(id) {
+    try {
+      const result = await this.databases.deleteDocument({
+        databaseId: config.appwriteDatabaseId,
+        collectionId: config.appwriteCollectionId,
+        documentId: id,
+      });
+
+      if (result) {
+        console.log(result);
+        return result;
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
